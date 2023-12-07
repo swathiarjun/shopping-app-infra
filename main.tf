773c009306c81d473f3f24211539b6819433175f
swathi <swathi@gmail.com>
2023-12-07 09:42:38 +0000

    securitygroup frontend created

--- main.tf
+++ main.tf
@@ -7,4 +7,41 @@ resource "aws_key_pair" "auth_key" {
 
   }
 }
+resource "aws_security_group" "frontend" {
+
+  name        = "${var.project_name}-${var.project_env}-frontend"
+  description = "${var.project_name}-${var.project_env}-frontend"
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}-${var.project_env}-frontend"
+    project = var.project_name
+    env     = var.project_env
+    owner   = var.project_owner
+  }
+}
 
