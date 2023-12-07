f700b9411e3c1dc6a2f8f7da10868cbbf9369e98
swathi <swathi@gmail.com>
2023-12-07 09:55:46 +0000

    securitygroup remoteaccess created

--- main.tf
+++ main.tf
@@ -44,4 +44,32 @@ resource "aws_security_group" "frontend" {
     owner   = var.project_owner
   }
 }
+resource "aws_security_group" "remote-access" {
+
+  name        = "${var.project_name}-${var.project_env}-remote-access"
+  description = "${var.project_name}-${var.project_env}-remote-access"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}-${var.project_env}-remote-access"
+    project = var.project_name
+    env     = var.project_env
+    owner   = var.project_owner
+  }
+}
+
 
