c106548f8af8549a0ffac6e9757aab96fdd0d39c
EC2 Default User <ec2-user@ip-172-31-38-196.ap-south-1.compute.internal>
2023-12-23 18:59:20 +0000

    Version 1 infra corrected

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_key_pair" "auth_key" {
 
   key_name   = "${var.project_name}-${var.project_env}"
-  public_key = file("redmi-prod.pub")
+  public_key = file("shopping-key.pub")
   tags = {
     Name = "${var.project_name}-${var.project_env}"
     project = var.project_name
